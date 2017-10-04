cce7db895102f79d9ab07f9b64a5023fe3c1d38b
Temak1 <Artes1123@yandex.ru>
2017-10-04 19:13:28 +0300

    1.0

---
+++ Documents/Python/PetroBot.py
@@ -0,0 +1,117 @@
+import discord
+import asyncio
+import json
+import os
+
+client = discord.Client()
+BOT_HELP = '''Доступные команды:
+**!@help** - выводит это сообщение
+**!@add** - добавляет видео в список (не более одного видео за раз) данного канала
+**!@poll** - запускает голосование за видео (пока только среди двух пользователей)
+**!@show** - показывает пригодный для копирования список видео данного канала
+**!@clear** - очищает список данного канала
+'''
+
+@client.event
+async def on_ready():
+	print('Logged in as ' + client.user.name + ' with id ' + client.user.id)
+	print('-----')
+
+"""@client.event
+async def on_resumed():
+	print('Resumed in as ' + client.user.name + ' with id ' + client.user.id)
+	print('-----')"""
+
+"""@client.event
+async def on_message(message):
+	print("check 1")
+	if message.content.startswith('!@check'):
+		print("check 2")
+		channel = message.channel
+		await channel.send('Send me that reaction, mate')
+		def check(reaction, user):
+			return user == message.author and str(reaction.emoji) == ''
+		try:
+			reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+		except asyncio.TimeoutError:
+			await channel.send('')
+		else:
+			await channel.send('')"""
+
+@client.event
+async def on_message(message):
+	if message.channel.name == 'general':
+		file_name = 'list_file.txt'
+	else:
+		file_name = 'test_file.txt'
+	print(file_name + ' choosen')
+
+	if message.content.startswith('!@help'):
+		await client.send_message(message.channel, BOT_HELP)
+		print('Выведена помощь')
+
+	elif message.content.startswith('!@add'):
+		if not os.path.isfile('list_file.txt'):
+			list_file_names = []
+			print('Файл ' + 'list_file.txt' + ' создан')
+		else:
+			with open ('list_file.txt', 'r') as list_file:
+				list_file_names = json.load(list_file)
+		list_file_names.append(message.content[5:].strip()) #.split('\n')
+		await client.add_reaction(message, '✅') #White check mark
+		print('Добавлено видео ' + message.content[5:].strip())
+
+	elif message.content.startswith('!@show'):
+		if not os.path.isfile(str(file_name)):
+			await client.send_message(message.channel, 'список видео данного канала пуст')
+			return
+		with open (str(file_name), 'r') as list_file:
+			list_file_names = json.load(list_file)
+		i = 0
+		string = 'Список видео:\n```\n'
+		while i < len(list_file_names):
+			#string = string + str(i+1) + '. ' + list_file_names[i]
+			string = string  + list_file_names[i]
+			i = i + 1
+			string = string + '\n'
+		string = string + '```'
+		await client.send_message(message.channel, string)
+		print('Показан список')
+
+	elif message.content.startswith('!@poll'):
+		await client.add_reaction(message, '✅') #White check mark
+		await client.add_reaction(message, '❌') #Cross Mark
+		print('Устроено голосование за видео' + message.content[5:].strip())
+
+	elif message.content.startswith('!@clear'):
+		os.remove(file_name)
+		await client.add_reaction(message, '✅')
+		print('Список данного канала удалён')
+	# elif message.content.startswith('!@clrchannel'):
+	# 	counter = 0
+	# 	async for msg in client.logs_from(message.channel, limit=1000):
+	# 		if msg.author == client.user:
+	# 			await client.delete_message(msg)
+	# 			counter += 1
+	# 	print('You have {} messages.'.format(counter))
+
+
+# Событие срабатывает только если реакция появилась под последним сообщением!
+@client.event
+async def on_reaction_add(reaction, user):
+	#print('1')
+	if (not reaction.me and reaction.message.author != user):
+		if reaction.emoji == '✅': #White check mark
+			with open ('list_file.txt', 'r') as list_file:
+				list_file_names = json.load(list_file)
+			list_file_names.append(message.content[5:].strip())
+			with open('list_file.txt', 'w') as list_file:
+				json.dump(list_file_names, list_file)
+			await client.remove_reaction(reaction.message, '❌', me) #removing #Cross Mark
+			print('Видео прошло голосование')
+
+		elif reaction.emoji == '❌': #Cross Mark
+			await client.remove_reaction(reaction.message, '✅', me) #removing White check mark
+			print('Видео не прошло голосование')
+
+client.run('MzU4OTU0Njc4MDIyODMyMTI4.DLQAoQ.tD2IwZLsvOs8BSeLYQYwXe9AE2k')
